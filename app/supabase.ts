e36af475a28440f91a5bbf87ac3c0253d741c15a
Rennan Dutra <rennandutraa@gmail.com>
2026-06-19 22:25:58 -0300

    Usar chaves do env no Supabase

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://loxglinjlzxgonrcyxjk.supabase.co";
-const supabaseKey = "sb_publishable_D3x2aglnhZorDf49AqlCKQ__cP821G4";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
